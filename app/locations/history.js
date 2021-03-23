19baeed097c10ef2d1bca7b35623ff66eaa03f33
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-03-23 12:07:10 +0100

    make locale prefixes dynamic

--- app/locations/history.js
+++ app/locations/history.js
@@ -1,17 +1,33 @@
 import HistoryLocation from '@ember/routing/history-location';
+import { inject as service } from '@ember/service';
 
 export default class MineLocation extends HistoryLocation {
+  @service intl;
+
   getURL() {
-    let superResult = super.getURL(...arguments);
-    superResult = superResult.replace(/^\/en/, "");
-    return superResult;
+    let path = super.getURL(...arguments);
+    path = this._stripLocalePrefix(path);
+    return path;
   }
 
   formatURL(path) {
     let superResult = super.formatURL(...arguments);
-    if (!superResult.startsWith("/en")) {
-      superResult = `/en${superResult}`;
+    if (!superResult.startsWith(`/${this.intl.primaryLocale}/`)) {
+      let routeSuffix = superResult.startsWith("/") ? superResult : `/${superResult}`;
+      superResult = `/${this.intl.primaryLocale}${routeSuffix}`;
     }
     return superResult;
   }
+
+  _stripLocalePrefix(path) {
+    for (let locale of this.intl.locales) {
+      let prefix = buildLocalePrefixRegexp(locale);
+      path = path.replace(prefix, "");
+    }
+    return path;
+  }
+}
+
+function buildLocalePrefixRegexp(locale) {
+  return new RegExp(`^\/${locale}\/`);
 }
