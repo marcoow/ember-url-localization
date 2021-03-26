047034c369813d84173d706313639675d03b7b8f
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-03-26 09:38:06 +0100

    forward translation

--- app/locations/history.js
+++ app/locations/history.js
@@ -1,6 +1,44 @@
 import HistoryLocation from '@ember/routing/history-location';
 import { inject as service } from '@ember/service';
 
+const DEFAULT_LOCALE = "en-us";
+
+//const TRANSLATIONS = {
+//  "es-es": {
+//    ["songs", "canciones"]: {
+//      ["library", "biblioteca"]: {
+//        DYNAMIC_SEGMENT: {
+//          ["reviews", "criticas"]: {}
+//        }
+//      }
+//    }
+//  }
+//};
+
+const TRANSLATIONS = {
+  "es-es": {
+    "songs": {
+      value: "canciones",
+      children: {
+        "library": {
+          value: "biblioteca",
+          children: {
+            isDynamicSegment: true,
+            children: {
+              "reviews": {
+                value: "ciriticas"
+              }
+            },
+            "about": {
+              value: "sobre"
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
 export default class MineLocation extends HistoryLocation {
   @service intl;
 
@@ -13,6 +51,10 @@ export default class MineLocation extends HistoryLocation {
 
   formatURL() {
     let url = super.formatURL(...arguments);
+    if (this.intl.primaryLocale !== DEFAULT_LOCALE) {
+      let translatedPaths = this._translatePaths(url);
+      console.log(translatedPaths);
+    }
     // TODO: translate url to this.intl.currentLocale
     let localePrefix = buildLocalePrefixRegexp(this.intl.primaryLocale);
     if (!url.match(localePrefix)) {
@@ -28,6 +70,23 @@ export default class MineLocation extends HistoryLocation {
       path = path.replace(prefix, "");
     }
     return path;
+  }
+
+  _translatePaths(url) {
+    let translations = TRANSLATIONS[this.intl.primaryLocale];
+    let paths = url.split("/").filter((path) => path.length > 0);
+    let translatedPaths = paths.reduce((acc, path) => {
+      let translationData = translations[path.toLowerCase()];
+      if (!translationData && translations.isDynamicSegment) {
+        translationData = translations;
+        acc.push(path);
+      } else {
+        acc.push(translationData.value);
+      }
+      translations = translationData.children;
+      return acc;
+    }, []);
+    return translatedPaths.join("/")
   }
 }
 
